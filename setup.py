92b0faa4dddcf970a726abadb5e6b56454048d0a
yifvs <128681381+yifvs@users.noreply.github.com>
2023-09-18 12:09:46 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,1 @@
-from setuptools import setup
 
-setup(
-    name='main_streamlit.py',
-    version='1.0',
-    install_requires=[
-        'plotly==5.16.1',
-        'langchain==0.0.288',
-    ],
-)
